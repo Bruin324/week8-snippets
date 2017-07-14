0185b2320003a697d89d13b41f705eeaefbb5690
Bruin324 <bruin324@gmAIL.COM>
2017-07-14 14:00:15 -0400

    initial setup commit v2

--- program.js
+++ program.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const bodyParser = require('bodyParser');
+const mustache = require('mustache-express');
 
 
+const userController = require('user-controller');
+const snippetController = require('snippet-controller')
 
 const application = express();
+
+application.engine('mustache', mustache());
+
+application.set('views', './views');
+application.set('view engine', 'mustache');
 
 
 application.use(bodyParser.json());
 application.use(bodyParser.urlencoded());
+
+application.use(userController);
+application.use(snippetController);
